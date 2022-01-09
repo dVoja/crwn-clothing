cecd904665e5b24adc14eb27605281e46ab663b8
dVoja <vojadanilovic@yahoo.com>
2022-01-08 21:47:22 -0500

    updating app with a shop page, collection pewview component and collection item component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import { Switch, Route } from 'react-router-dom';
 
 
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
-const HatsPage = ()=>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 
 
 function App() {
@@ -20,7 +17,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   )
